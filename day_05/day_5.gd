b9d8dfdce63b4a1b971f69db29eab77ff50af78f
Jon Peck <jpeck@fluxsauce.com>
2025-12-06 14:57:29 -0500

    Day 5 part 1

---
+++ day_05/day_5.gd
@@ -0,0 +1,50 @@
+class_name Day5
+extends Node
+
+
+static func parse_fresh_ranges(raw: String) -> Array[Array]:
+	var rows: PackedStringArray = raw.split("\n")
+	var fresh_ranges: Array[Array] = []
+	var parse: bool = true
+	for row in rows:
+		if row == "":
+			parse = false
+		if parse:
+			var row_values: PackedStringArray = row.split("-")
+			var start: int = int(str(row_values[0]))
+			var end: int = int(str(row_values[1]))
+			fresh_ranges.append([start, end])
+	return fresh_ranges
+
+
+static func parse_available_ids(raw: String) -> Array[int]:
+	var rows: PackedStringArray = raw.split("\n")
+	var available: Array[int] = []
+	var ready: bool = false
+	for row in rows:
+		if ready:
+			available.append(int(row))
+		elif row == "":
+			ready = true
+	return available
+
+
+static func check_ranges_if_fresh(id: int, fresh_ranges: Array[Array]) -> bool:
+	for fresh_range: Array[int] in fresh_ranges:
+		if is_fresh(id, fresh_range):
+			return true
+	return false
+
+
+static func is_fresh(id: int, fresh_range: Array) -> bool:
+	return id >= fresh_range[0] and id <= fresh_range[1]
+
+
+static func count_fresh(raw: String) -> int:
+	var fresh_ranges: Array[Array] = parse_fresh_ranges(raw)
+	var available_ids: Array[int] = parse_available_ids(raw)
+	var fresh: Array[int] = []
+	for id: int in available_ids:
+		if check_ranges_if_fresh(id, fresh_ranges):
+			fresh.append(id)
+	return fresh.size()
---
+++ test/unit/test_day_5.gd
@@ -0,0 +1,25 @@
+extends GutTest
+
+
+func test_parse_fresh_ranges() -> void:
+	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
+	var actual: Array[Array] = Day5.parse_fresh_ranges(input)
+	var expected: Array[Array] = [[3, 5], [10, 14], [16, 20], [12, 18]]
+	assert_eq(actual, expected)
+
+
+func test_parse_available_ids() -> void:
+	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
+	var actual: Array[int] = Day5.parse_available_ids(input)
+	var expected: Array[int] = [1, 5, 8, 11, 17, 32]
+	assert_eq(actual, expected)
+
+
+func test_count_fresh_sample() -> void:
+	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
+	assert_eq(Day5.count_fresh(input), 3)
+
+
+func test_count_fresh() -> void:
+	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input.txt")
+	assert_eq(Day5.count_fresh(input), 563)
