564328eca67f42139d00570d36a7ae75e217c60c
Jon Peck <jpeck@fluxsauce.com>
2025-12-07 19:28:26 -0500

    Day5 complete

--- day_05/day_5.gd
+++ day_05/day_5.gd
@@ -2,9 +2,116 @@ class_name Day5
 extends Node
 
 
-static func parse_fresh_ranges(raw: String) -> Array[Array]:
+static func pack_fresh_range(index_a: int, ranges: Array) -> Array:
+	#print()
+	#print_debug("start: %s index: %s" % [ranges, index_a])
+
+	var ranges_size: int = ranges.size()
+	if index_a > (ranges_size - 1):
+		#print_debug("Invalid index, we're done")
+		return ranges
+
+	var a: Array = ranges[index_a]
+
+	for index_b in range(ranges_size):
+		var b: Array = ranges[index_b]
+
+		# Ignore itself.
+		if index_a == index_b:
+			#print_debug("%s: ignoring itself" % [a])
+			continue
+
+		# A ends and B starts one away or is exactly the same.
+		if (a[1] < b[0] && a[1] + 1 == b[0]) or (a[1] == b[0]):
+			var new_range: Array = [a[0], b[1]]
+			#print_debug(
+			#"%s,%s: A ends and B starts one away, combining into %s" % [a, b, new_range]
+			#)
+			ranges[index_a] = new_range
+			ranges.remove_at(index_b)
+			#print_debug("end: %", ranges)
+			return ranges
+
+		# B ends and A starts one away or is exactly the same
+		if (b[1] < a[0] && b[1] + 1 == a[0]) or (b[1] == a[0]):
+			var new_range: Array = [b[0], a[1]]
+			#print_debug(
+			#"%s,%s: B ends and A starts one away, combining into %s" % [a, b, new_range]
+			#)
+			ranges[index_a] = new_range
+			ranges.remove_at(index_b)
+			#print_debug("end: %", ranges)
+			return ranges
+
+		# A ends before B starts.
+		if a[1] <= b[0]:
+			#print_debug("%s,%s: A ends before B starts, ignoring" % [a, b])
+			continue
+
+		# A starts after B starts.
+		if a[0] >= b[1]:
+			#print_debug("%s,%s: A starts after B ends, ignoring" % [a, b])
+			continue
+
+		# A intersects with B; also covers A and B being the same ranges.
+		if a[0] <= b[0] && a[1] >= b[0] && a[1] <= b[1]:
+			var new_range: Array = [a[0], b[1]]
+			#print_debug("%s,%s: A intersects with B, combining into %s" % [a, b, new_range])
+			ranges[index_a] = new_range
+			ranges.remove_at(index_b)
+			#print_debug("end: %", ranges)
+			return ranges
+
+		# B intersects with A.
+		if b[0] <= a[0] && b[1] > a[0] && b[1] <= a[1]:
+			var new_range: Array = [b[0], a[1]]
+			#print_debug("%s,%s: B intersects with A, combining into %s" % [a, b, new_range])
+			ranges[index_a] = new_range
+			ranges.remove_at(index_b)
+			#print_debug("end: %", ranges)
+			return ranges
+
+		# A is completely within B.
+		if b[0] <= a[0] && b[1] >= a[1]:
+			#print_debug("%s,%s: A is completely within B, removing A" % [a, b])
+			ranges.remove_at(index_a)
+			#print_debug("end: %", ranges)
+			return ranges
+
+		# B is completely within A.
+		if a[0] <= b[0] && a[1] >= b[1]:
+			#print_debug("%s,%s: B is completely within A, removing B" % [a, b])
+			ranges.remove_at(index_b)
+			#print_debug("end: %", ranges)
+			return ranges
+
+	#print_debug("end (no change): %", ranges)
+	return ranges
+
+
+static func pack_fresh_ranges(ranges_source: Array) -> Array:
+	var ranges_target: Array = []
+	for index in range(ranges_source.size()):
+		ranges_target.append(ranges_source[index])
+		# Process it until there's no changes left.
+		var changed: bool = true
+		var old_size: int = ranges_target.size()
+		while changed:
+			# Intentionally checking the size every time.
+			for target_index in range(ranges_target.size()):
+				ranges_target = pack_fresh_range(target_index, ranges_target)
+				# Check if there's any differences.
+				var new_size: int = ranges_target.size()
+				if new_size == old_size:
+					changed = false
+
+	ranges_target.sort_custom(sort_by_first_element)
+	return ranges_target
+
+
+static func parse_fresh_ranges(raw: String) -> Array:
 	var rows: PackedStringArray = raw.split("\n")
-	var fresh_ranges: Array[Array] = []
+	var fresh_ranges: Array = []
 	var parse: bool = true
 	for row in rows:
 		if row == "":
@@ -17,9 +124,9 @@ static func parse_fresh_ranges(raw: String) -> Array[Array]:
 	return fresh_ranges
 
 
-static func parse_available_ids(raw: String) -> Array[int]:
+static func parse_available_ids(raw: String) -> Array:
 	var rows: PackedStringArray = raw.split("\n")
-	var available: Array[int] = []
+	var available: Array = []
 	var ready: bool = false
 	for row in rows:
 		if ready:
@@ -29,15 +136,16 @@ static func parse_available_ids(raw: String) -> Array[int]:
 	return available
 
 
-static func check_ranges_if_fresh(id: int, fresh_ranges: Array[Array]) -> bool:
-	for fresh_range: Array[int] in fresh_ranges:
+static func check_ranges_if_fresh(id: int, fresh_ranges: Array) -> bool:
+	for fresh_range: Array in fresh_ranges:
 		if is_fresh(id, fresh_range):
 			return true
 	return false
 
-static func get_first_range_index(id: int, fresh_ranges: Array[Array]) -> int:
+
+static func get_first_range_index(id: int, fresh_ranges: Array) -> int:
 	var fresh_ranges_size: int = fresh_ranges.size()
-	for index:int in range(fresh_ranges_size):
+	for index: int in range(fresh_ranges_size):
 		if is_fresh(id, fresh_ranges[index]):
 			return index
 	# Impossible to get here
@@ -49,24 +157,22 @@ static func is_fresh(id: int, fresh_range: Array) -> bool:
 
 
 static func count_fresh(raw: String) -> int:
-	var fresh_ranges: Array[Array] = parse_fresh_ranges(raw)
-	var available_ids: Array[int] = parse_available_ids(raw)
-	var fresh: Array[int] = []
+	var fresh_ranges: Array = parse_fresh_ranges(raw)
+	var available_ids: Array = parse_available_ids(raw)
+	var fresh: Array = []
 	for id: int in available_ids:
 		if check_ranges_if_fresh(id, fresh_ranges):
 			fresh.append(id)
 	return fresh.size()
 
 
-static func count_from_ranges(fresh_ranges: Array[Array]) -> int:
-	var count:int = 0
-	var fresh_ranges_size:int = fresh_ranges.size()
-	for index in range(fresh_ranges_size):
-		var fresh_range: Array = fresh_ranges[index]
-		# See if it's unique.
-		# Update: can't do that.
-		for id in range(fresh_range[0], fresh_range[1] + 1):
-			var first_range_index:int = get_first_range_index(id, fresh_ranges)
-			if first_range_index == index:
-				count += 1
+static func count_from_packed_ranges(packed_ranges: Array) -> int:
+	var count: int = 0
+	var packed_ranges_size: int = packed_ranges.size()
+	for index in range(packed_ranges_size):
+		count += (packed_ranges[index][1] - packed_ranges[index][0]) + 1
 	return count
+
+
+static func sort_by_first_element(a: Array, b: Array) -> bool:
+	return a[0] < b[0]
--- test/unit/test_day_5.gd
+++ test/unit/test_day_5.gd
@@ -3,15 +3,31 @@ extends GutTest
 
 func test_parse_fresh_ranges() -> void:
 	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
-	var actual: Array[Array] = Day5.parse_fresh_ranges(input)
-	var expected: Array[Array] = [[3, 5], [10, 14], [16, 20], [12, 18]]
+	var actual: Array = Day5.parse_fresh_ranges(input)
+	var expected: Array = [[3, 5], [10, 14], [16, 20], [12, 18]]
 	assert_eq(actual, expected)
+
+
+func test_pack_fresh_range() -> void:
+	assert_eq(Day5.pack_fresh_range(0, [[1, 3], [2, 4], [10, 12]]), [[1, 4], [10, 12]])
+	assert_eq(Day5.pack_fresh_range(0, [[1, 5], [2, 4], [10, 12]]), [[1, 5], [10, 12]])
+	assert_eq(Day5.pack_fresh_range(0, [[2, 6], [1, 3], [10, 12]]), [[1, 6], [10, 12]])
+	assert_eq(Day5.pack_fresh_range(0, [[10, 14], [12, 18]]), [[10, 18]])
+	assert_eq(Day5.pack_fresh_range(0, [[1, 2], [3, 4]]), [[1, 4]])
+	assert_eq(Day5.pack_fresh_range(0, [[3, 4], [1, 2]]), [[1, 4]])
+	assert_eq(Day5.pack_fresh_range(0, [[1, 3], [3, 4]]), [[1, 4]])
+	assert_eq(Day5.pack_fresh_range(0, [[3, 4], [1, 3]]), [[1, 4]])
+
+
+func test_pack_fresh_ranges() -> void:
+	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
+	assert_eq(Day5.pack_fresh_ranges(Day5.parse_fresh_ranges(input)), [[3, 5], [10, 20]])
 
 
 func test_parse_available_ids() -> void:
 	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
-	var actual: Array[int] = Day5.parse_available_ids(input)
-	var expected: Array[int] = [1, 5, 8, 11, 17, 32]
+	var actual: Array = Day5.parse_available_ids(input)
+	var expected: Array = [1, 5, 8, 11, 17, 32]
 	assert_eq(actual, expected)
 
 
@@ -24,10 +40,22 @@ func test_count_fresh() -> void:
 	var input: String = Helpers.get_text_file_content("res://day_05/day_5_input.txt")
 	assert_eq(Day5.count_fresh(input), 563)
 
+
 func test_count_from_ranges_sample() -> void:
-	var input: Array[Array] = Day5.parse_fresh_ranges(Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt"))
-	assert_eq(Day5.count_from_ranges(input), 14)
+	var input: Array = Day5.parse_fresh_ranges(
+		Helpers.get_text_file_content("res://day_05/day_5_input_sample.txt")
+	)
+	var packed: Array = Day5.pack_fresh_ranges(input)
+	#for row:Array in packed:
+	#print(row)
+	assert_eq(Day5.count_from_packed_ranges(packed), 14)
+
 
 func test_count_from_ranges() -> void:
-	var input: Array[Array] = Day5.parse_fresh_ranges(Helpers.get_text_file_content("res://day_05/day_5_input.txt"))
-	assert_eq(Day5.count_from_ranges(input), 14)
+	var input: Array = Day5.parse_fresh_ranges(
+		Helpers.get_text_file_content("res://day_05/day_5_input.txt")
+	)
+	var packed: Array = Day5.pack_fresh_ranges(input)
+	#for row:Array in packed:
+	#print(row)
+	assert_eq(Day5.count_from_packed_ranges(packed), 338693411431456)
